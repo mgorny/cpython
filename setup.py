e0a51b708b02e29a4682e89ff725549447cc9257
Mike Gilbert <floppym@gentoo.org>
2018-01-05 13:32:45 -0500

    setup.py: exit with non-zero status on failure
    https://bugs.gentoo.org/show_bug.cgi?id=281968
    https://bugs.python.org/issue6731

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ if pds_env in os.environ:
 else:
     disable_ssl = 0
 
+exit_status = 0
+
 def add_dir_to_list(dirlist, dir):
     """Add the directory 'dir' to the list 'dirlist' (after any relative
     directories) if:
@@ -358,7 +360,10 @@ class PyBuildExt(build_ext):
             print_three_column([ext.name for ext in mods_disabled])
             print()
 
+        global exit_status
+
         if self.failed:
+            exit_status = 1
             failed = self.failed[:]
             print()
             print("Failed to build these modules:")
@@ -366,6 +371,7 @@ class PyBuildExt(build_ext):
             print()
 
         if self.failed_on_import:
+            exit_status = 1
             failed = self.failed_on_import[:]
             print()
             print("Following modules built successfully"
@@ -2364,6 +2370,7 @@ def main():
           scripts = ["Tools/scripts/pydoc3", "Tools/scripts/idle3",
                      "Tools/scripts/2to3", "Tools/scripts/pyvenv"]
         )
+    sys.exit(exit_status)
 
 # --install-platlib
 if __name__ == '__main__':
